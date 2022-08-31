3aed408841161459e2f22cd89675ebcee68e40cd
Nathalia Meza <nathaliameza@hotmail.com>
2022-08-31 17:26:28 -0500

    Commit Inicial

---
+++ TallerMecanico/TallerMecanico/Models/PersonaModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TallerMecanico.Models
+{
+    public class PersonaModel
+    {
+        public int IdPersona { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public string? Identificacion { get; set; }
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public string? Nombre { get; set; }
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public string? Apellido { get; set; }
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public string? Nacimiento { get; set; }
+
+    }
+}
